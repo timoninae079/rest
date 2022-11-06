006ba441fb044f3df3eb1c57071d29cbedb37802
ЕленаПрекрасная <timoninae079@gmail.com>
2022-11-06 20:41:00 +0300

    'RUB'

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -20,7 +20,7 @@ class MobileBankApiTestV3 {
                 .body("", hasSize(3))
                 .body("[0].currency", equalTo("RUB"))
                 .body("[1].currency", equalTo("USD"))
-                .body("[2].currency", equalTo("RUR"))
+                .body("[2].currency", equalTo("RUB"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
         ;
     }
